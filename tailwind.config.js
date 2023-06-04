674d695cde6a41157bf6f3e5c05858ef7f60d1e3
Mahdi-Fayyaz <118967843+Mahdi-Fayyaz@users.noreply.github.com>
2023-06-04 12:17:08 +0330

    add file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        snow: {
+          low: "#47d1d1",
+          dark: "#1e3a8a",
+          light: "#005266",
+        },
+    },
+  },
+  plugins: [],
+}
+}
